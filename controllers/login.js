4f7999716dc8fda3acb476e1de444cc454c6186e
Rafael Mamede <r.silvamamede@gmail.com>
2022-05-31 11:00:34 -0300

    Adicionando a porta ao nome do servico

--- controllers/login.js
+++ controllers/login.js
@@ -6,7 +6,7 @@ const { userInstance } = require('../models/User');
 const login = ({ nickname, port }) => {
     return new Promise((resolve, reject) => {
         exec("docker service create " +
-            `--name ${nickname} --publish ${port}:${port} `+
+            `--name ${nickname}-${port} --publish ${port}:${port} `+
             `--env REACT_APP_NICKNAME=${nickname} --env PORT=${port} --env REACT_APP_SERVER_ENDPOINT=http://192.168.100.10:3000 --env REACT_APP_REFRESH_TIME=5000 ` +
             "192.168.100.10:5000/chat:latest",
         (error, stdout, stderr) => {
